86c9430c12af9a7993e5d62e9d64c590e4176671
Abdelrahman <AbdelrahmanAlasal@gmail.com>
2023-06-21 03:13:46 +0300

    Categories CRUD

--- Ecommerce/Controllers/CategoryController.cs
+++ Ecommerce/Controllers/CategoryController.cs
@@ -1,12 +1,99 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ecommerce.Data;
+using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.Controllers
 {
     public class CategoryController : Controller
     {
+        private ApplicationDbContext _db { get; set; }
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
+        {
+            var list = _db.Categories.ToList();
+            return View(list);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+
+            TempData["sucess"] = "A Category has been created";
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+          
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+
+            Category? obj = _db.Categories.Find(id);
+      
+            if (obj == null)
+            {
+                return NotFound();
+            }
+ 
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            _db.Categories.Update(category);
+            _db.SaveChanges();
+            TempData["sucess"] = "A Category has been Updated";
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+
+            Category? obj = _db.Categories.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Remove(obj);
+            _db.SaveChanges();
+            TempData["sucess"] = "A Category has been Deleted";
+            return RedirectToAction("index");
+        }
+
     }
 }
--- Ecommerce/Models/Category.cs
+++ Ecommerce/Models/Category.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Ecommerce.Models
 {
@@ -11,8 +13,14 @@ namespace Ecommerce.Models
         public int Id { get; set; }
 
         [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30 , ErrorMessage = "The Category name is too long")]
         public string Name { get; set; }
-        public int DisplayOrder { get; set; }
 
+        [Required(ErrorMessage = "The Display order Field is Required")]
+        [DisplayName("Display Order")]
+        [Range(1,100 , ErrorMessage = "Please Insert Display order must be between 1 and 100 ") ]
+        
+        public int DisplayOrder { get; set; }
     }
 }
